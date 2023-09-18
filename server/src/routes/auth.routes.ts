a187ce1ec2d6d1463e57b5430ace4475f2c54266
Nicolas Schneider <nicolas.schneider@epitech.eu>
2023-09-18 16:47:05 +0200

    feat: docker-compose forprod

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/auth.controller"; // Change path accordingly
+import { registerUser, loginUser } from "../controllers/auth.controller";
 
 const router = Router();
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,10 +5,28 @@ import router from "./routes";
 import { errorHandler } from "./middlewares/errorHandler";
 import { notFoundHandler } from "./middlewares/notFoundHandler";
 
+require("dotenv").config();
+
+if (process.env.NODE_ENV === "production") {
+  process.env.DATABASE_URL = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.DB_HOST_PROD}:${process.env.DB_PORT}/${process.env.POSTGRES_DB}?schema=public`;
+} else {
+  process.env.DATABASE_URL = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.POSTGRES_DB}?schema=public`;
+}
+
 const app = express();
 const PORT = 8080;
 
-app.use(cors());
+// CORS settings
+const corsOptions = {
+  origin:
+    process.env.NODE_ENV === "production"
+      ? "https://pwa.nicolas-schneider.fr"
+      : "http://localhost:3000",
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 
 app.use("/", router);
