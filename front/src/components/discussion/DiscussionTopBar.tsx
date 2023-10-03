8814944d593a65d20018b70009d3f21d78da6392
Nicolas Schneider <59870022+SchneiderNicolas@users.noreply.github.com>
2023-10-03 16:34:09 +0200

    release 0.0.6 (#20)
    * feat: mobile front

--- front/src/components/discussion/DiscussionTopBar.tsx
+++ front/src/components/discussion/DiscussionTopBar.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
+import { useDiscussionContext } from '../../contexts/DiscussionContext';
 import { useResponsive } from '../../hooks/useResponsive';
 import { User } from '../../types/discussionTypes';
 import Tooltip from '../Tooltip';
@@ -9,18 +12,38 @@ type DiscussionTopBarProps = {
 };
 
 const DiscussionTopBar = ({ title, users }: DiscussionTopBarProps) => {
-  const isMobile = useResponsive();
+  const navigate = useNavigate();
+  const { isMobile } = useResponsive();
+  const { resetViewState } = useDiscussionContext();
+
+  const handleBackClick = () => {
+    resetViewState();
+    navigate('/');
+  };
+
   return (
-    <div
-      className={`z-40 sticky top-0 bg-white border-b border-gray-200 ${
-        !isMobile ? ' border-l border-gray-200 ml-96 border-r' : ''
-      }`}
-    >
+    <div className={'z-40 sticky top-0 bg-white border-b border-gray-200'}>
+      {isMobile && (
+        <button
+          onClick={handleBackClick}
+          className="absolute top-0 left-0 cursor-pointer flex items-center p-2 text-base font-semibold text-violet-500 rounded-br-md hover:bg-violet-100"
+          aria-label="Go back"
+        >
+          <FiArrowLeft size={24} color={'#8b5cf6'} />
+        </button>
+      )}
       <div className="flex flex-col items-center px-4 py-2">
-        <div className="text-lg font-bold text-center mb-1">{title}</div>
+        <div className="text-lg font-bold text-center mb-4 md:mb-2">
+          {title}
+        </div>
         <div className="flex flex-wrap space-x-2 self-start">
           {users.map((user) => (
-            <Tooltip key={user.id} tooltipText={user.email} position="top">
+            <Tooltip
+              isMobile={isMobile}
+              key={user.id}
+              tooltipText={user.email}
+              position="top"
+            >
               <div className="px-3 py-1 rounded-md bg-violet-400 text-white font-medium text-sm mb-1 cursor-pointer">
                 {user.name || user.email}
               </div>
--- front/src/components/discussion/new/NewDiscussionTopBar.tsx
+++ front/src/components/discussion/new/NewDiscussionTopBar.tsx
@@ -4,7 +4,9 @@ import { User } from '../../../types/discussionTypes';
 import UserSearch from './UserSearch';
 import SelectedUser from './SelectedUser';
 import Tooltip from '../../Tooltip';
-import { FiEdit2 } from 'react-icons/fi';
+import { FiArrowLeft, FiEdit2 } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
+import { useDiscussionContext } from '../../../contexts/DiscussionContext';
 
 type NewDiscussionTopBarProps = {
   title: string;
@@ -22,8 +24,10 @@ const NewDiscussionTopBar = ({
   onUserRemove,
 }: NewDiscussionTopBarProps) => {
   const [isEditingTitle, setIsEditingTitle] = useState(false);
-  const isMobile = useResponsive();
+  const { isMobile } = useResponsive();
   const inputRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+  const { resetViewState } = useDiscussionContext();
 
   const handleTitleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
@@ -31,16 +35,27 @@ const NewDiscussionTopBar = ({
     }
   };
 
+  const handleBackClick = () => {
+    resetViewState();
+    navigate('/');
+  };
+
   return (
-    <div
-      className={`z-40 sticky top-0 bg-white border-b border-gray-200 ${
-        !isMobile ? ' border-l border-gray-200 ml-96 border-r' : ''
-      }`}
-    >
+    <div className={'z-40 sticky top-0 bg-white border-b border-gray-200'}>
+      {isMobile && (
+        <button
+          onClick={handleBackClick} // use handleBackClick function
+          className="absolute top-0 left-0 cursor-pointer flex items-center p-2 text-base font-semibold text-violet-500 rounded-br-md hover:bg-violet-100"
+          aria-label="Go back"
+        >
+          <FiArrowLeft size={24} color={'#8b5cf6'} />{' '}
+        </button>
+      )}
       <div className="flex flex-col items-center px-4 py-2">
         <div className="text-lg font-bold text-center mb-1 flex items-center">
           {isEditingTitle ? (
             <input
+              autoComplete="off"
               ref={inputRef}
               value={title}
               onChange={(e) => onTitleChange(e.target.value)}
@@ -53,7 +68,7 @@ const NewDiscussionTopBar = ({
             <>
               {title}
               <Tooltip
-                isMobile={isMobile.isMobile}
+                isMobile={isMobile}
                 tooltipText="Edit Title"
                 position="right+"
               >
