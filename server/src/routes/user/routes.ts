f781726b9b7907c19b827e5585b6641af826ace2
Nicolas Schneider <59870022+SchneiderNicolas@users.noreply.github.com>
2023-10-13 18:26:23 +0200

    release 0.0.9 fix (#38)
    * feat: removed debug route (#37)

--- server/src/routes/user/routes.ts
+++ server/src/routes/user/routes.ts
@@ -1,10 +1,8 @@
 import express from "express";
 import { searchUsers } from "../../controllers/user/searchUsers";
-import { getAllUsers } from "../../controllers/user/allUser";
 
 export const router = express.Router();
 
 router.get("/", searchUsers);
-router.get("/all", getAllUsers);
 
 export default router;
