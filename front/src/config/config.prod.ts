5096b4e2ae313d0d23a91e3e6f8b4da34e3ca401
Nicolas Schneider <59870022+SchneiderNicolas@users.noreply.github.com>
2023-10-16 16:50:39 +0200

    release 0.1.0 fix (#44)
    * fix: prod config (#43)

--- front/src/config/config.prod.ts
+++ front/src/config/config.prod.ts
@@ -1,6 +1,6 @@
 const config = {
-  API_BASE_URL: 'http://localhost:8080',
-  BASE_URL: 'http://localhost:3000/',
+  API_BASE_URL: 'https://api.pwa.nicolas-schneider.fr',
+  BASE_URL: 'https://pwa.nicolas-schneider.fr',
   PUBLIC_VAPID_KEY:
     'BDwN10CsEWgEYtF1fGEc_Y7iXOPseiYp-N9-l2KyNmp6RV4F7M7nVPBeuVxHvV63T4PXXfh-XKTRq4FuBcKajNQ',
 } as const;
